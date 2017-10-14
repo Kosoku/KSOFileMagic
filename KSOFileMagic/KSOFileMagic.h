3f7e6261d68d1f27dca3f9f7ff339f50e977d481
William Towe <willbur1984@gmail.com>
2017-10-14 11:38:02 -0400

    add file submodule add initial target

---
+++ KSOFileMagic/KSOFileMagic.h
@@ -0,0 +1,19 @@
+//
+//  KSOFileMagic.h
+//  KSOFileMagic
+//
+//  Created by William Towe on 10/14/17.
+//  Copyright © 2017 Kosoku Interactive, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KSOFileMagic.
+FOUNDATION_EXPORT double KSOFileMagicVersionNumber;
+
+//! Project version string for KSOFileMagic.
+FOUNDATION_EXPORT const unsigned char KSOFileMagicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KSOFileMagic/PublicHeader.h>
+
+
